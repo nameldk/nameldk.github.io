59883d5ec2ce96dca4a722ef1149129f9595ebd5
dengke.li <2009lidengke@163.com>
2019-01-24 11:10:45 +0800

    add:test webpush

---
+++ test/webpush/config.js
@@ -0,0 +1,4 @@
+window.GoogleSamples = window.GoogleSamples || {};
+window.GoogleSamples.Config = window.GoogleSamples.Config || {
+  gcmAPIKey: 'AAAAiMqaVq4:APA91bEAQ_J0vfd5dciqpnLeJGjA3Ea4bjHroJ2N2x_EoNpieEuU8HoXmPaCoxoldR-BHT4biuS49TZyzjGhnbjF__HDRubgMloo5HBOCODoHUKFkPWyKRu_aKLxB8zXF_LHYbvEzeko'
+};
